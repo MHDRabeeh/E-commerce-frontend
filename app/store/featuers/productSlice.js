86f9d00e21ec4f2860585159ddbcfffe3fcfc0c9
MHDRabeeh <rabirabz001@gmail.com>
2025-06-21 13:22:07 +0530

    add some changes

--- app/store/featuers/productSlice.js
+++ app/store/featuers/productSlice.js
@@ -1,14 +1,48 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// product fetching
+
+export const fetchProduct = createAsyncThunk("listProduct", async () => {
+  try {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/get`
+    );
+
+    return data.products;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/////     product slice ////
 
 export const productSlice = createSlice({
-    name:"products",
-    initialState:{
-        products :[]
-    },
-    reducers:{
+  name: "products",
+  initialState: {
+    products: [],
+    loading:false,
+    error:null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+   builder.addCase(fetchProduct.pending,(state)=>{
+    state.loading = true;
+   })
+   .addCase(fetchProduct.fulfilled,(state,action)=>{
+    state.loading = false;
+    console.log(action);
+    
+    state.products = action.payload;
+   }).addCase(fetchProduct.rejected,(state,action)=>{
+    state.loading = false;
+    state.error = action.error.message
+   })
 
-    }
+    
+  },
+});
 
-})
-export const {}= productSlice.actions;
+export const {} = productSlice.actions;
 export default productSlice.reducer;
--- app/store/store.js
+++ app/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from './featuers/productSlice'
 
+
 export const store = configureStore({
   reducer: {
     products:productReducer
   },
 });
+
+
